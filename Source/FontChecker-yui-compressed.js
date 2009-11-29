548ec75fc158b16433115a53073ab1e443cf2133
David Walsh <davidwalsh83@gmail.com>
2009-11-29 17:53:59 -0600

    Initial Commit

---
+++ Source/FontChecker-yui-compressed.js
@@ -0,0 +1,18 @@
+/*
+---
+description:     FontChecker
+
+authors:
+  - David Walsh (http://davidwalsh.name)
+
+license:
+  - MIT-style license
+
+requires:
+  core/1.2.1:   '*'
+
+provides:
+  - FontChecker
+...
+*/
+var FontChecker=new Class({Implements:[Options],options:{fakeFont:"__RUBBUSH_FONT__",testString:"abcdefghijklmnopqrstuvwxyz"},initialize:function(a){this.setOptions(a);},check:function(d){var b=new Element("span",{styles:{visibility:"hidden","font-family":this.options.fakeFont},html:this.options.testString}).inject(document.body);var c=b.getCoordinates().width;b.setStyle("font-family",d);var a=b.getCoordinates().width;b.dispose();return(c!==a);}});
---
+++ Source/FontChecker.js
@@ -0,0 +1,51 @@
+/*
+---
+description:     FontChecker
+
+authors:
+  - David Walsh (http://davidwalsh.name)
+
+license:
+  - MIT-style license
+
+requires:
+  core/1.2.1:   '*'
+
+provides:
+  - FontChecker
+...
+*/
+var FontChecker = new Class({
+	/* implements */
+	Implements: [Options],
+	/* options */
+	options: {
+		fakeFont: '__RUBBUSH_FONT__',
+		testString: 'abcdefghijklmnopqrstuvwxyz'
+	},
+	/* initialization */
+	initialize: function(options) {
+		//set options
+		this.setOptions(options);
+	},
+	/* a method that does whatever you want */
+	check: function(desiredFont) {
+		/* create a hidden element */
+		var element = new Element('span',{
+			styles: {
+				visibility: 'hidden',
+				'font-family': this.options.fakeFont
+			},
+			html: this.options.testString
+		}).inject(document.body);
+		/* apply a fake font, get width */
+		var width = element.getCoordinates().width;
+		/* apply desired font */
+		element.setStyle('font-family', desiredFont);
+		var new_width = element.getCoordinates().width;
+		/* take our temp element out of the DOM */
+		element.dispose();
+		/* compare widths to see check if font is available */
+		return (width !== new_width);
+	}
+});
